bee79504438aed1e2cc8e7ccc035fe3367c4c6d2
kasparsg <kasparsg@192.168.43.241>
2018-05-01 09:22:45 +0300

    Clean base project with added summernote, pusher API and master.blade.php file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,6 +22,8 @@ and open the template in the editor.
         <div class="container">
             @yield('content')
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>       
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
         <script>
