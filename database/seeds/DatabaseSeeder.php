f6407d5f67066cfc4bd878547ba6e8c9120372e5
kasparsg <kasparsg@192.168.43.241>
2018-06-08 12:19:24 +0300

    Default note types seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      DB::table('note_type')->insert([
+        'type_name' => 'default',
+      ]);
+      DB::table('note_type')->insert([
+        'type_name' => 'to-do',
+      ]);
+      DB::table('note_type')->insert([
+        'type_name' => 'collaborative ',
+      ]);
     }
 }
